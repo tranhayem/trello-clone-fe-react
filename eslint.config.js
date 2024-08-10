590b289df8fce756a64486df3f03970004a6f9b7
thy <mailcuathy@gmail.com>
2024-08-10 23:08:22 +0700

    chore: enhance ESLint config with additional rules and MUI restrictions

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,75 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 
 export default [
   {
-    files: ['**/*.{js,jsx}'],
-    ignores: ['dist'],
+    files: ["**/*.{js,jsx}"],
+
+    ignores: ["dist"],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
-    settings: { react: { version: '18.3' } },
+
+    settings: { react: { version: "18.3" } },
+
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
+
     rules: {
       ...js.configs.recommended.rules,
+
+      // React
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": 0,
+      "react/display-name": 0,
+
+      // MUI
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["@mui/*/*/*"],
+        },
+      ],
+
+      // Common
+      "no-console": 1,
+      "no-lonely-if": 1,
+      "no-unused-vars": 1,
+      "no-trailing-spaces": 1,
+      "no-extra-boolean-cast": 0,
+      "no-multi-spaces": 1,
+      "no-multiple-empty-lines": 1,
+      "space-before-blocks": ["error", "always"],
+      "object-curly-spacing": [1, "always"],
+      indent: ["warn", 2],
+      "array-bracket-spacing": 1,
+      "linebreak-style": 0,
+      "no-unexpected-multiline": "warn",
+      "keyword-spacing": 1,
+      "comma-spacing": 1,
+      "arrow-spacing": 1,
     },
   },
-]
+];
