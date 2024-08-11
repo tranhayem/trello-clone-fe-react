61a8634f9d86ac024587b646bbe2c6d4e9e2f74c
thy <mailcuathy@gmail.com>
2024-08-12 02:49:16 +0700

    feat: implement routing with react-router-dom and add basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import Box from "@mui/material/Box";
+import { useState } from "react";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+
+import Header from "./components/header/Navbar";
+
+import NotFound from "./pages/NotFound";
+import Login from "./pages/auth/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Workspaces from "./pages/workspaces/Workspaces";
 
 function App() {
-  return <Box></Box>;
+  const [loggedIn, setLoggedIn] = useState(true);
+  return (
+    <Router>
+      {loggedIn && <Header />}
+      <Routes>
+        <Route
+          path="/"
+          element={loggedIn ? <Dashboard /> : <Navigate to="/login" />}
+        />
+
+        <Route
+          path="/login"
+          element={loggedIn ? <Navigate to="/" /> : <Login />}
+        />
+
+        <Route
+          path="/workspaces"
+          element={loggedIn ? <Workspaces /> : <Navigate to="/login" />}
+        />
+
+        <Route
+          path="*"
+          element={loggedIn ? <NotFound /> : <Navigate to="/login" />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
