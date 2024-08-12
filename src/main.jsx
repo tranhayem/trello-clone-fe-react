4e3687ffc3778230ac52d9a7553b8bac4598530d
thy <mailcuathy@gmail.com>
2024-08-13 04:12:54 +0700

    fix: wrap App component with StrictMode for development

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-
 import theme from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
-  <CssVarsProvider theme={theme} defaultMode="system">
-    <CssBaseline />
-    <App />
-  </CssVarsProvider>
+  <StrictMode>
+    <CssVarsProvider theme={theme} defaultMode="system">
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </StrictMode>
 );
